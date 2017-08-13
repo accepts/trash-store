5762e66e984df74e0e74a8b809262a23973172b2
accepts <accepts@ya.ru>
2017-08-13 11:47:35 +0300

    method2

--- src/repository/RepoImpl.java
+++ src/repository/RepoImpl.java
@@ -16,6 +16,10 @@ public class RepoImpl {
     public static void method1(){
         System.out.println("1111");
     }
-    
+
+    public static void method2(){
+        System.out.println("2222");
+    }
+
 
 }
