83b8f0b158815dd17480a3b8ed320fa47782e909
accepts <accepts@ya.ru>
2017-08-13 13:08:30 +0300

    NON validate method add

--- src/service/Validator.java
+++ src/service/Validator.java
@@ -1,7 +1,14 @@
 package service;
 
 public class Validator {
+
     public void validate(String str){
         System.out.println("String is valid");
     }
+
+    public void validate2(String str){
+        System.out.println("String is ERROR");
+    }
+
+
 }
